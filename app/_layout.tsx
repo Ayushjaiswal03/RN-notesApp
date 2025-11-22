fe887dfa696744a482adb257f1e6d3bcd8e1ea8b
Ayushjaiswal03 <ayush60000@gmail.com>
2025-11-23 03:05:01 +0530

    Layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,54 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import React, { useEffect } from "react";
+import { Stack } from "expo-router";                //  stack wrapper
+import { StatusBar } from "expo-status-bar";       // mobile's status bar 
+import useAuthStore from "../store/authStore";     //  Zustand auth store
+import { ActivityIndicator, View } from "react-native";
+import { useRouter } from "expo-router";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  
+  const loadUserFromStorage = useAuthStore((s) => s.loadUserFromStorage); // load persisted user
+  const user = useAuthStore((s) => s.user);                             // current user (or null)
+  const loading = useAuthStore((s) => s.loading);                       // loading flag
+  const router = useRouter();  
+
+
+  useEffect(() => {
+    loadUserFromStorage(); // loads user into store (async)
+  }, [loadUserFromStorage]);
+
+  useEffect(() => {
+   
+    if (user) {
+     
+      router.replace("/notes");
+    } else {
+      router.replace("/login");
+    }
+  }, [user, router]);
+
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
+        <Stack.Screen name="notes" options={{ headerShown: false }} />
+        
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
